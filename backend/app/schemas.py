faf5322f4c97304a394ec34a058fd714b760fc54
Zach5824 <kazokoth58@gmail.com>
2026-07-29 14:44:47 +0300

    authentication schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -3,3 +3,24 @@ from typing import Optional
 from datetime import datetime
 from .models import UserRole
 
+# Auth Schemas
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    role: Optional[UserRole] = UserRole.user
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    role: UserRole
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
